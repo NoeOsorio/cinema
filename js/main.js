ef1cf4ab1303dff0e14792be13c52ab84bf6490e
Noe Osorio <osorionoe@gmail.com>
2024-07-05 15:43:41 -0600

    Add horarios.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("Hello World!")
+const movies = [
+    {
+        title: 'Wall-e',
+        duration: '2h 22min',
+        genre: ['Animación', 'Aventura', 'Familia'],
+    },
+    {
+        title: 'Spiderman 2',
+        duration: '2h 55min',
+        genre: ['Acción', 'Aventura', 'Superheores'],
+    },
+    {
+        title: 'Avengers: Infinite War',
+        duration: '2h 45min',
+        genre: ['Acción', 'Aventura', 'Superheores'],
+    }
+]
+
+function getQueryParam(param) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+function getMovie() {
+    const index = getQueryParam('movie');
+    return movies[index];
+}
